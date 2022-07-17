043af82a9df271565512d5d9754ab96cf0c36f5a
ItaloCobains <monokatchaleger@gmail.com>
2022-07-16 23:04:11 -0300

    styles Home Page

--- pages/[tenant]/index.tsx
+++ pages/[tenant]/index.tsx
@@ -1,10 +1,21 @@
-import styles from "./../styles/Home.module.css";
+import styles from "./../../styles/Home.module.css";
 
 const Home = () => {
     return (
         <div className={styles.container}>
             <header className={styles.header}>
-
+                <div className={styles.headerTop}>
+                    <div className={styles.headerTopLeft}>
+                        <div className={styles.headerTitle}>
+                            Seja Bem-Vindo (a) 👋
+                        </div>
+                        <div className={styles.headerSubTitle}>
+                            O que deseja para hoje ?
+                        </div>
+                    </div>
+                    <div className={styles.headerTopRight}></div>
+                </div>
+                <div className={styles.headerBottom}>Busca</div>
             </header>
         </div>
     );
