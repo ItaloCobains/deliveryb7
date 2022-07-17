492ebd29a26251b1ddd9a6bb05858f20d459bd29
ItaloCobains <monokatchaleger@gmail.com>
2022-07-16 22:49:21 -0300

    init repo

---
+++ pages/[tenant]/index.tsx
@@ -0,0 +1,13 @@
+import styles from "./../styles/Home.module.css";
+
+const Home = () => {
+    return (
+        <div className={styles.container}>
+            <header className={styles.header}>
+
+            </header>
+        </div>
+    );
+};
+
+export default Home;
