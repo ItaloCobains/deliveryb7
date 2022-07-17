72d2e1365498e825d8075a4a0a040d10d8bcadf1
ItaloCobains <monokatchaleger@gmail.com>
2022-07-17 14:45:02 -0300

    more components

--- pages/[tenant]/index.tsx
+++ pages/[tenant]/index.tsx
@@ -1,37 +1,50 @@
-import { SearchInput } from "../../components/SearchInput";
 import styles from "./../../styles/Home.module.css";
 
-const Home = () => {
-    const handleSearch = (searchValue: string) => [
-        console.log(`Vc esta buscando por: ${searchValue}`),
-    ];
+import { Banner } from "../../components/Banner"
+import { SearchInput } from "../../components/SearchInput";
+import { ProductItem } from '../../components/ProductItem';
 
-    return (
-        <div className={styles.container}>
-            <header className={styles.header}>
-                <div className={styles.headerTop}>
-                    <div className={styles.headerTopLeft}>
-                        <div className={styles.headerTitle}>
-                            Seja Bem Vindo (a) 👋
-                        </div>
-                        <div className={styles.headerSubTitle}>
-                            O que deseja para hoje ?
-                        </div>
-                    </div>
-                    <div className={styles.headerTopRight}>
-                        <div className={styles.MenuBottom}>
-                            <div className={styles.menuButtomLine}></div>
-                            <div className={styles.menuButtomLine}></div>
-                            <div className={styles.menuButtomLine}></div>
-                        </div>
-                    </div>
-                </div>
-                <div className={styles.headerBottom}>
-                    <SearchInput mainColor="#fb9400" onSearch={handleSearch} />
-                </div>
-            </header>
+
+const Home = () => {
+  const handleSearch = (searchValue: string) => {
+    console.log(`Vc esta buscando por: ${searchValue}`);
+  }
+
+  return (
+    <div className={styles.container}>
+      <header className={styles.header}>
+        <div className={styles.headerTop}>
+          <div className={styles.headerTopLeft}>
+            <div className={styles.headerTitle}>
+              Seja Bem Vindo (a) 👋
+            </div>
+            <div className={styles.headerSubTitle}>
+              O que deseja para hoje ?
+            </div>
+          </div>
+          <div className={styles.headerTopRight}>
+            <div className={styles.MenuBottom}>
+              <div className={styles.menuButtomLine}></div>
+              <div className={styles.menuButtomLine}></div>
+              <div className={styles.menuButtomLine}></div>
+            </div>
+          </div>
         </div>
-    );
+        <div className={styles.headerBottom}>
+          <SearchInput mainColor="#fb9400" onSearch={handleSearch} />
+        </div>
+      </header>
+
+      <Banner />
+
+      <div className={styles.grid}>
+        <ProductItem />
+        <ProductItem />
+        <ProductItem />
+        <ProductItem />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
